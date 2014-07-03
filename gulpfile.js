fec253e8f0e3c12542a177496a92773e230ef7d2
索丘 <zengyue.yezy@alibaba-inc.com>
2014-07-03 13:22:02 +0800

    init project

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var rename = require('gulp-rename');
+var less = require('gulp-less');
+var minifyCSS = require('gulp-minify-css');
+var watch = require('gulp-watch');
+
+
+gulp.task('clean', function(){
+  return gulp.src('dist/*',{read: false})
+  .pipe(clean());
+});
+
+gulp.task('watch', function(){
+  gulp.watch('src/*.less', ['less']);
+});
+
+
+gulp.task('less', function(){
+  return gulp.src([
+    'src/*.less'
+  ])
+  .pipe(less())
+  .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('default', ['clean'], function(){
+  gulp.start('less');
+});
